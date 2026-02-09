04349812e68070c8047d60af6bfab307868a310f
juanfelipeordonezsalina-svg <juanfelipe.ordonezsalina@gmail.com>
2026-02-09 13:41:35 -0500

    Add files via upload

---
+++ Corte1/Practica2/Proyecto2.java
@@ -0,0 +1,13 @@
+
+package proyecto2;
+
+
+public class Proyecto2 {
+public int x, y, z;
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    }
+    
+}
